a0291ffd29360f335bcda92d0cd0b94888586a25
Julian <me@julian.pro>
2020-07-26 12:46:22 +0200

    mail configuration

--- config/config.go
+++ config/config.go
@@ -2,9 +2,31 @@ package config
 
 import (
 	"errors"
+	"time"
 
 	"github.com/spf13/viper"
 )
+
+// EmailProviderConfiguration holds email related configs
+type EmailProviderConfiguration struct {
+	Disabled bool `json:"disabled"`
+}
+
+// EmailContentConfiguration holds the configuration for emails, both subjects and template URLs.
+type EmailContentConfiguration struct {
+	Recovery    string `json:"recovery"`
+	EmailChange string `json:"email_change" split_words:"true"`
+}
+
+// SMTPConfiguration is the SMTP config for the Mailer
+type SMTPConfiguration struct {
+	MaxFrequency time.Duration `json:"max_frequency" split_words:"true"`
+	Host         string        `json:"host"`
+	Port         int           `json:"port,omitempty" default:"587"`
+	User         string        `json:"user"`
+	Pass         string        `json:"pass,omitempty"`
+	AdminEmail   string        `json:"admin_email" split_words:"true"`
+}
 
 // WebConfiguration keeps information for our WebService
 type WebConfiguration struct {
@@ -23,9 +45,15 @@ type DBConfiguration struct {
 
 // Configuration holds all the configuration that applies to the shortener application.
 type Configuration struct {
-	Secret string           `yaml:"Secret"`
-	Web    WebConfiguration `yaml:"Web"`
-	DB     DBConfiguration  `yaml:"DB"`
+	Secret string            `yaml:"Secret"`
+	Web    WebConfiguration  `yaml:"Web"`
+	DB     DBConfiguration   `yaml:"DB"`
+	SMTP   SMTPConfiguration `yaml:"smtp"`
+	Mailer struct {
+		Subjects  EmailContentConfiguration `yaml:"subjects"`
+		Templates EmailContentConfiguration `yaml:"templates"`
+		URLPaths  EmailContentConfiguration `yaml:"url_paths"`
+	} `yaml:"mailer"`
 }
 
 // LoadGlobal loads the configuration from file and env variables.
