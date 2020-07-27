77f8a83f5b9837df9581e769a07451b3ddf6e292
Julian <me@julian.pro>
2020-07-27 15:08:39 +0200

    open storage connection

--- cmd/serve.go
+++ cmd/serve.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/juliankoehn/mchurl/api"
 	"github.com/juliankoehn/mchurl/config"
+	"github.com/juliankoehn/mchurl/storage"
 	"github.com/juliankoehn/mchurl/stores"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -17,6 +18,12 @@ var serveCmd = cobra.Command{
 }
 
 func serve(config *config.Configuration) {
+	db, err := storage.Dial(config)
+	if err != nil {
+		logrus.Fatalf("Error opening database: %+v", err)
+	}
+	defer db.Close()
+
 	store, err := stores.New(&config.DB)
 	if err != nil {
 		logrus.Fatal(err)
